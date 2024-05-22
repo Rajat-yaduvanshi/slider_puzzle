c70516515cfa429272190a39edb1333c277dcbf6
ryadav29 <rajat.yadav@vvdntech.in>
2024-05-22 16:09:51 +0530

    play again button added

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { flattenArray } from "../utils"; // Import the flattenArray function from a utilities file
+import { flattenArray } from "../utils"; 
 import image1 from "../assets/image_part_001.jpg";
 import image2 from "../assets/image_part_002.jpg";
 import image3 from "../assets/image_part_003.jpg";
@@ -9,10 +9,20 @@ import image5 from "../assets/image_part_005.jpg";
 import image6 from "../assets/image_part_006.jpg";
 import image7 from "../assets/image_part_007.jpg";
 import image8 from "../assets/image_part_008.jpg";
+import image9 from "../assets/image_part_009.jpg";
 import image10 from "../assets/image_part_010.jpg";
-import "../index.css"; // Import the external stylesheet
+import "../index.css";
 
 const Board = ({ onMove, onComplete, onReset, moves, puzzle, complete }) => {
+  const [currentImage, setCurrentImage] = useState(image10);
+
+  useEffect(() => {
+    if (complete) {
+      setCurrentImage(image9); 
+    } else {
+      setCurrentImage(image10);
+    }
+  }, [complete]);
   const getImageSource = (value) => {
     switch (value) {
       case 1:
@@ -32,11 +42,12 @@ const Board = ({ onMove, onComplete, onReset, moves, puzzle, complete }) => {
       case 8:
         return image8;
       case 0:
-        return image10;
+        return currentImage;
       default:
         return null;
     }
   };
+  
 
   const movePiece = (x, y) => {
     if (!complete) {
@@ -106,6 +117,9 @@ const Board = ({ onMove, onComplete, onReset, moves, puzzle, complete }) => {
       <div className="ResetButton">
         <button onClick={onReset} disabled={complete}>
           Reshuffle
+        </button>
+        <button onClick={onReset}>
+          Play Again
         </button>
       </div>
     </div>
